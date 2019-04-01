1742b181f33ac1785e04dcb6e48ada48c6759d1e
Frederic Mendes <fredmanumendes@gmail.com>
2019-04-01 12:31:54 +0100

    fullapp

--- App.js
+++ App.js
@@ -1,21 +1,55 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Image, ImageBackground } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <ImageBackground
+         source={require('./assets/zen.jpg')}
+         style={styles.container}
+      >
+        <Text
+        style={styles.title}
+        >Hello World</Text>
+        <Text
+        style={styles.subtitle}
+        >Be connected everywhere</Text>
+        <Image
+          source={require('./assets/donald.jpg')}
+          style={styles.avatar}
+        />
+        <Text
+        style={styles.subtitle}
+        >Join the network</Text>
+
+      </ImageBackground>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+  avatar: {
+    width: 120,
+    height: 120,
+    borderRadius: 60,
   },
+
+  container: {
+    width: '100%',
+    height: '100%',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  title: {
+    color: "white",
+    fontSize: 50,
+  },
+
+    subtitle:{
+    color: "#ffff",
+    fontSize: 30,
+    }
 });
